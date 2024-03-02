dc9cb5eb7de7546c34656bc8179ba9cb8ff8ef8f
Noah Wager <noahwager@gmail.com>
2024-03-01 16:28:29 -0800

    Add rest of conent to pages
    Tidy up and add the rest of the reflections.
    
    Signed-off-by: Noah Wager <noahwager@gmail.com>

--- src/routes/junior/+page.svelte
+++ src/routes/junior/+page.svelte
@@ -16,3 +16,99 @@
 </ColumnContainer>
 
 <hr>
+
+<ImageBlock title="Autumn 2022">
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        I had the opportunity to study abroad in Paris for Fall Quarter and I made so many new friends from different colleges across the country. We were a small program (only 40 people!) so we all became very close. In this photo featured, we decided to go to a pumpkin patch since it was October! It wasn't at all like the ones in America, but it was still very cool. They had a restaurant with amazing food.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="right">
+    <p>
+        In one of my classes, a Global Civil Rights class, we had to write a biographical essay on a social activist. I chose Tarana Burke who coined the term "me too" and started a revolution against sexual violence. This essay was inspiring to write because it allowed me to learn more about a woman who really help redefine the landscape surrounding sexual harassment.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        Even though I was abroad, I still had responsibilities as the PR/Marketing Chair for my fraternity, Alpha Phi Omega. I wanted to revamp our Instagram and give it some more flavor, so I made various posts throughout the week. Featured to the left, you can see some community service highlights, a playlist, alumni feature, and some features of our members.
+	</p>
+    <br style="clear:both" />
+
+    <h3>Reflection</h3>
+
+    <p>
+        I started off my school year by flying abroad to Paris! I went with the CIEE Open Block Program, which allowed me to collaborate with students from other schools rather than a UW program. This program also only lasted 2/3 of the quarter rather than a whole quarter. Fortunately, my boyfriend also went with me, so I wasn't totally alone. The majority of the students lived in tiny apartments in Montrouge, and I had a roommate named Michelle, who was from Pennsylvania. I took two classes abroad (which accounts for about 12 credits) and got to know pretty much everyone in the program. My closest friend abroad is Sofia, who fortunately goes to Seattle University, so I get to see her in the States as well!
+	</p>
+    <p>
+        Learning in a different country was very interesting. I took AP French in high school, but I really had the chance to practice it while I was ordering food, talking to Parisians, etc. My classes were in English, but sometimes the professors didn't know the English word for something, so they switched to French. I took the metro to class every morning and got to know which lines were the best very well. I also identified the best cafes around the school area for when I had a break between classes.
+	</p>
+    <p>
+        The program also took some trips to other places, one of them being an overnight trip to Bordeaux. We got to go to the wine museum and try some of the finest wine they had (the French rarely drink water). I also just explored in general with my friends. We went to the highlights like the Eiffel Tower, Louvre, Musee d'Orsay, etc. We also went to places like the Catacombes, Montmartre, the 6th arrondissement for thrifting, etc. I really had a lot of fun exploring abroad.
+	</p>
+    <p>
+        My biggest issue while abroad was getting sick. I caught strep about halfway through the trip and had to navigate the French healthcare system all by myself. I went on antibiotics, but once I was done with those my strep came back. I ended up going back to the ER, and they wouldn't give me antibiotics despite me asking for them. My strep got worse and morphed into an ear infection, which left me semi-deaf for several months after I returned home. It was the scariest and loneliest time for me. I was so homesick and didn't feel well, and just wanted to go home. Getting sick abroad is not fun at all. My Dad ended up flying out for a surprise visit in order to take care of me because I was cooped up in my tiny apartment with medicine in a language I didn't fully understand with no one there.
+	</p>
+    <p>
+        Despite the hardships, I'm glad I went abroad, and I think it was a formative experience for learning independence and culture.
+    </p>
+</ImageBlock>
+
+<ImageBlock title="Winter 2023">
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        For my Soc 371: Criminology, I had to use the theories we examined in class to analyze a case, and I chose the Elizabeth Smart Case and Strain Theory. Elizabeth Smart was a girl who was abducted in the early 2000s and was returned home alive. She was forced to be smart and cunning in order to survive. I used strain theory though to analyze the perpetrator, Brian David Mitchell. I ended up getting 100% on this essay.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="right">
+    <p>
+        Once back in the states, all I did was perform. This photo is from my favorite show I've done so far at Funhouse and it was a Goth Valentine's theme. I also did shows with MESH Gala, Sunset Tavern, High Dive, etc.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        My most significant achievement from the quarter was getting 100% in my Criminology class. I was so ecstatic to get such a good grade in a class that I was really passionate about.
+	</p>
+    <br style="clear:both" />
+
+    <h3>Reflection</h3>
+
+    <p>
+        Winter 2023 was my first quarter back from abroad, and I had to adjust accordingly. I really enjoyed the classes I was in though, and made lots of time for my friends that I had missed during autumn. My favorite class by far was Criminology. I have always had an interest in Criminology, and if UW had offered that as a major I would've declared that. I even considered enrolling in the FBI for a time. Still, I enjoyed Criminology and loved Ann Frost as a professor. I ended up getting 100% in the class! Goes to show that if you're passionate about the subject, you'll do well. I also took an honors class on Women in Greek and Roman Antiquity. I love everything greek myth, and I was interested in to learn about women's roles. I also made some good friends in the class, and had lots of fun learning about the Greeks and Romans.
+	</p>
+    <p>
+        I had a lot of performances in winter, but my favorite by far is my performance at Funhouse. It was an all ages performance with Seattle bands Instant Crush, Cherry Ferrari, and Lovely Colors. The theme was goth valentine because it was February 13. Not only did most of my fraternity show up, but a bunch of fans I didn't know I had also showed up! It was exhilarating to perform and see people singing along to my songs. A girl came up to me afterward and showed me that she was my #1 Spotify listener, and I had never met her before. It was such a crazy experience but it pushed me to pursue my music.
+	</p>
+    <p>
+        Winter is always a dull quarter because the sun is rarely up, and students are generally reasonably depressed. This quarter wasn't too bad compared to most of my other winter quarters, but it still felt slow compared to Autumn and Spring. Now, I only have one more winter quarter left of my college career, and it's getting bittersweet.
+	</p>
+</ImageBlock>
+
+<ImageBlock title="Spring 2023">
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        Never having written a script before, I was pleasantly surprised by the way my first music video turned out! It took us all day to film at Ballard High School, and working with all of these people interested in my work was really heartwarming. I'm proud of the product we produced.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="right">
+    <p>
+        After three years of diligent work, I was finally elected President of Alpha Phi Omega. I did not run unopposed, and I was so nervous I wouldn't get the position, but I did and I'm so happy. One of the proudest moments of my life.
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="desc" class="left">
+    <p>
+        I got to continue being an actor in LUX this quarter and scored three roles, which is crazy! I got cast as the lead in a film that hasn't finished, the role of a sorority girl named Willa, and in the film, I included Jess in Minutiae. Filming as Jess was rough because I had never had to have a love interest before, but working with Conan was fun, and I found it a good experience.
+	</p>
+    <br style="clear:both" />
+
+    <h3>Reflection</h3>
+
+    <p>
+        This was a successful quarter for me, where I felt like I made a lot of progress and was happy about the work I accomplished. I did acting, music, school, etc. The classes I was in were fascinating, and I made a lot of great connections. I took a Sociology of Race class and met a girl named Shyal, who's been in many of my classes. We hit it off, which is one of the first times I've made a friend in a class and not in a club. I spent lots of time with APO people doing community service and fellowship events, which is always a good feeling. I liked being able to work on my acting; I spent every week improving my audition skills and learning to dissect a script. I even got an acting teacher (which is a crazy story).
+	</p>
+    <p>
+        I went to Emerald City Comic Con in the spring, and I had the chance to meet one of my favorite actors, Sherilyn Fenn, from Twin Peaks and Gilmore Girls. I was with my Dad, and he bragged about me to her. She liked hearing that I was an acting minor and asked me many questions (promptly ignoring my Dad, which was entertaining). She wanted me enough to give me her information, saying she thought I had a lot of potential and shouldn't waste it. It's wild to say that one of my favorite actors is my teacher and that she sees potential in me. It makes me want to jump up in glee.
+	</p>
+    <p>
+        Spring quarter always interests me because it feels like the end. I bloom in spring because I'm shaking off the seasonal depression from winter. This was a successful spring, and I look forward to seeing how my last year treated me.
+	</p>
+</ImageBlock>
--- src/routes/senior/+page.svelte
+++ src/routes/senior/+page.svelte
@@ -16,3 +16,34 @@
 </ColumnContainer>
 
 <hr>
+
+
+<ImageBlock title="Autumn 2023">
+    <img src="{base}/images/butters.jpeg" alt="zone one song" class="left">
+    <p>
+        In Honors 210 “Empire and Poesis of Place,” we read a book called Zone One by Colson Whitehead. We ended the course creating projects based on themes of the course, and I decided to write a song. I really loved the apocalyptic themes in the novel, so I wrote my song based off of that. My favorite line in the song was one at the end of the novel: “I can’t swim but I’ll learn how.”
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="bloom, vol. 1" class="right">
+    <p>
+        I’ve been diligently working on new music, and in December I released an EP titled “bloom, vol .1.” I was unbelievably proud of the work that went into it. I had an EP release party at Seattle Records on the Ave, and it had a good turn out with lots of friends and even strangers. My favorite song on it is “why do I feel this way.”
+	</p>
+    <br style="clear:both" />
+    <img src="{base}/images/butters.jpeg" alt="Connor + Chloe" class="left">
+    <p>
+        My most strenuous class was Directing. Our final project, was to write, direct, and stage a play of our own creation. I wrote a play that was inspired by my love for horror, and final girls. I cast my friends Connor and Chloe to play the main characters. I ended up getting a pretty good grade on the play!
+	</p>
+    <br style="clear:both" />
+
+    <h3>Reflection</h3>
+
+    <p>
+        I went into my last autumn quarter really excited for all the work to occur. I thought my classes sounded interesting, I was president of my fraternity, and my band was doing okay. Unfortunately, it ended up being a really hard time for me mentally. One thing about me, is I have a very dry sarcastic sense of humor. One of the people in my fraternity was offended by that sense of humor, and we ended up having a falling out (I tried to apologize, but she wouldn’t accept that). It escalated to me being reported to the board for my behavior. Eventually, no one wanted work with me anymore and started essentially bullying me. I sent in my resignation, letting them know that I felt bullied, and didn’t return since. 
+	</p>
+    <p>
+        This sent me into a depression, further highlighted by more friends deciding not to talk to me anymore (because I put in “too much effort”). I was feeling very aimless, and alone. A community that I’ve had since freshman year was gone. I felt utterly alone. 
+	</p>
+    <p>
+        I took that pain though, and channeled it into my music and the friends that stayed and fought for me. I started really looking inward as to why I make friendships like these, and sought out answers within myself for how I could be better moving forward. I started to heal, even though classes were hard. My Directing class was incredibly stressful. It was a lot of work, and the teacher wasn’t always the most receptive. I also struggled in my honors class, where all the students were fairly convinced that the teacher actually had no idea what the class was about. My favorite class was Murder with Professor Ann Frost. It was extremely interesting, and I loved being able to tell people “I’m taking a class called Murder!” Overall, I ended the year happy, and that’s what was important for me. 
+	</p>
+</ImageBlock>
